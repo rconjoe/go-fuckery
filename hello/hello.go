68764e95e238f2cd221772573d21e403a363606e
rconjoe <root@trog.codes>
2025-04-25 14:04:43 -0700

    adjust for error handled greetings

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example/greetings"
 )
 
 func main() {
-	message := greetings.Hello("asshole")
+	// set log entry prefix and a flag to disable printing the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("asshole")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
