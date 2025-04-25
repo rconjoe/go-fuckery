16474018844cfb23a418baef7c234ec37aedd463
rconjoe <root@trog.codes>
2025-04-25 16:14:44 -0700

    introduce Hellos function to try maps

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -15,6 +15,26 @@ func Hello(name string) (string, error) {
 	return message, nil
 }
 
+// Hellos returns a map that associates each of the names people with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	// make a map of key:value string:string
+	messages := make(map[string]string)
+
+	// in this for loop, range returns two values: the index and copy of the value.
+	// we don't need the index so we can use blank identifier
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		// assign a message to each name
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 // randomFormat returns one of a set of greeting messages.
 func randomFormat() string {
 	// a slice of message formats
